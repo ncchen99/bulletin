888df5a61891b6dfc56454e638603929ce0d8cbd
ncchen99 <ncchen99@gmail.com>
2021-05-30 21:50:33 +0800

    🌞

--- assets/js/bulletin.js
+++ assets/js/bulletin.js
@@ -19,6 +19,69 @@ function init_database() {
   firebase.analytics();
 }
 
+function statusChangeCallback(response) {
+  // Called with the results from FB.getLoginStatus().
+  console.log("statusChangeCallback");
+  console.log(response); // The current login status of the person.
+  if (response.status === "connected") {
+    // Logged into your webpage and Facebook.
+    testAPI();
+  } else {
+    // Not logged into your webpage or we are unable to tell.
+    document.getElementById("status").innerHTML =
+      "Please log " + "into this webpage.";
+    console.log("Please log " + "into this webpage.");
+  }
+}
+
+function checkLoginState() {
+  // Called when a person is finished with the Login Button.
+  FB.getLoginStatus(function (response) {
+    // See the onlogin handler
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "1209542359498398",
+    cookie: true, // Enable cookies to allow the server to access the session.
+    xfbml: true, // Parse social plugins on this webpage.
+    version: "v10.0", // Use this Graph API version for this call.
+  });
+
+  FB.getLoginStatus(function (response) {
+    // Called after the JS SDK has been initialized.
+    statusChangeCallback(response); // Returns the login status.
+  });
+};
+
+function getUserProfileImg(userId) {
+  console.log("Welcome!  Fetching your information.... ");
+    FB.api(
+      "/" + userId + "/picture",
+      "GET",
+      { redirect: "false", height: 200, width: 200 },
+      function (img_response) {
+        console.log(img_response);
+      }
+    );
+}
+function testAPI() {
+  // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+  console.log("Welcome!  Fetching your information.... ");
+  FB.api("/me", function (response) {
+    FB.api(
+      "/" + response.id + "/picture",
+      "GET",
+      { redirect: "false", height: 200, width: 200 },
+      function (img_response) {
+        console.log(img_response);
+      }
+    );
+  });
+}
+
 //get random int
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
